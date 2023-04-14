3b1e80a5603a00db67e4b57f4946450031c3391d
Maxim Alenchikov <maxim.alenchikov@gmail.com>
2023-04-14 10:34:12 +0300

    codewars

---
+++ 7 kyu/divideAndConquer.js
@@ -0,0 +1,11 @@
+function divCon(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === Number(arr[i])) {
+            sum = sum + arr[i];
+        } else {
+            sum = sum - Number(arr[i]);
+        }
+    }
+    return sum;
+}
